919db4d8f539de16a69fa09f32363b9dedc56619
勇 <757639768@qq.com>
2022-02-08 16:30:36 +0800

    feat：添加useMount和useDebounced两hook

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import {SearchPanel} from "./secrch-panel";
 import {List} from "./list";
 import {useEffect, useState} from "react";
 import qs from "qs";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce} from "../../utils";
 
 const apiUrl=process.env.REACT_APP_API_URL
 
@@ -18,26 +18,36 @@ export const ProjectListScreen = () => {
     const [list,setList]=useState([])
 
     const [users, setUsers]=useState([])
+
+    const debouncedParam = useDebounce(param,2000)
+
 //列表发生变化时发出异步请求，通过useEffect函数刷新页面
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {//fetch函数获得后台数据
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {//fetch函数获得后台数据
             if(response.ok){
                 setList(await response.json()) //通过useEffect函数对state进行更改
             }
         })
-    },[param]) //每次deps发生改变后会执行useEffect函数，从而执行setState函数从而变更刷新状态
+    },[debouncedParam]) //每次deps发生改变后会执行useEffect函数，从而执行setState函数从而变更刷新状态
 
-    useEffect(()=>{
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async (response) => {
             if (response.ok){
                 setUsers(await response.json())
             }
         })
-    },[param])
+    })
 
     return <div>
         {/*{通过props传递fetch获得的后台数据}*/}
         <SearchPanel users={users} param={param} setParam={setParam}/>
       <List users={users} list={list}/>
     </div>
+}
+
+//hook只能在组件中运行或其他hook中运行
+export const useMount = (callback) => {
+  useEffect(()=>{
+        callback()
+  },[])
 }
